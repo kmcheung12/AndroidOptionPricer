9fdc95a1c27d5eb272315cefeb9c410972d7dd43
alan <kmcheung12@gmail.com>
2014-05-01 20:02:34 +0800

    Support up to 5 stocks for arithmetic basket option

--- src/com/comp7405/optionpricer/ArithmaticBasketActivity.java
+++ src/com/comp7405/optionpricer/ArithmaticBasketActivity.java
@@ -5,25 +5,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.*;
 import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.TextView;
 
-import static com.comp7405.optionpricer.StatisticHelper.matrixMul;
+public class ArithmaticBasketActivity extends Activity implements  OnClickListener, OnCheckedChangeListener, AdapterView.OnItemSelectedListener {
+	Button bCalculate;
+	EditText  etStrikePrice, etTimetoMaturity, etInterestRate,etPath;
+    RadioGroup rgOptionType, rgMCOption;
+    TextView tvResult;
 
-public class ArithmaticBasketActivity extends Activity implements  OnClickListener, OnCheckedChangeListener  {
-	Button bCalculate; 
-	EditText etStock1Price,etStock2Price, etStrikePrice, etTimetoMaturity, etSigma1,etSigma2, etInterestRate,etCovariance,etPath;
-	RadioGroup rgOptionType, rgMCOption;
-	TextView tvResult;
-	
+    private EditText[] etStockPrices , etSigmas;
+    private Spinner spinner;
+    private EditText[] etRhos;
+
 	private OptionType optionType;
     private PricerMethod method;
-	private double K,T,r, rho;
-    private int path;
-	private double[] sigma, spots;
+	private double K,T,r;
+    private int path, n;
+	private double[] sigmas, spots;
+    private double[][] rhos;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +34,7 @@ public class ArithmaticBasketActivity extends Activity implements  OnClickListen
 
 	@Override
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
-		switch (group.getId()){
+        switch (group.getId()){
 		case R.id.rgOption:
 			switch(checkedId){
 				case R.id.rbCall:
@@ -65,20 +65,34 @@ public class ArithmaticBasketActivity extends Activity implements  OnClickListen
 	public void onClick(View arg0) {
         try {
             OptionPricer pricer = new OptionPricer();
+            spots = new double[n];
+            sigmas = new double[n];
+            for (int i=0; i<n; i++) {
+                spots[i] = Double.parseDouble(etStockPrices[i].getText().toString());
+                sigmas[i] = Double.parseDouble(etSigmas[i].getText().toString());
+            }
 
-            double[][] matA = {{1,2},{3,4}};
-            double[][] result = matrixMul(matA,matA);
-            System.out.print(result);
 
+            rhos = new double[n][n];
 
-            spots = new double[] {Double.parseDouble(etStock1Price.getText().toString()),Double.parseDouble(etStock2Price.getText().toString())};
+            int count = 0;
+            for (int i =0; i<n; i++) {
+                for (int j =0; j<= i; j++) {
+                    rhos[i][j] = Double.parseDouble(etRhos[count].getText().toString());
+                    rhos[j][i] = rhos[i][j];
+                    count +=1;
+                }
+            }
+
             K = Double.parseDouble(etStrikePrice.getText().toString());
             T = Double.parseDouble(etTimetoMaturity.getText().toString());
-            sigma =  new double[]{Double.parseDouble(etSigma1.getText().toString()),Double.parseDouble(etSigma2.getText().toString())};
             r = Double.parseDouble(etInterestRate.getText().toString());
-            rho = Double.parseDouble(etCovariance.getText().toString());
             path = Integer.parseInt(etPath.getText().toString());
-            double[] Result = pricer.basketArithmetic(optionType, spots, K, T, sigma, r, new double[][]{{1,rho},{rho,1}}, path, method);
+
+//            double[][] rhos = new double[][] {{1.0000, 0.4000, 0.4500, 0.5000}, {0.4,1,0.5,0.6}, {0.45,0.5,1,0.65}, {0.5, 0.6, 0.65, 1}};
+
+//            double[] Result = pricer.basketArithmetic(optionType, new double[]{100,100,100,100}, 120, T, new double[] {0.3,0.2,0.1,0.1}, 0.05, rhos, path, method);
+            double[] Result = pricer.basketArithmetic(optionType, spots, K, T, sigmas, r, rhos, path, method);
             tvResult.setText("Option Price is " + Double.toString(Result[0]) + " With 95% confidence interval at " +Double.toString(Result[1]) + " and "  +Double.toString(Result[2]) );
         } catch (Exception e) {
             e.printStackTrace();
@@ -87,25 +101,93 @@ public class ArithmaticBasketActivity extends Activity implements  OnClickListen
 	}
 
 	public void initialize(){
-		bCalculate = (Button) findViewById(R.id.bCalculate);
-		bCalculate.setOnClickListener(this);
-		etStock1Price = (EditText) findViewById(R.id.etStock1Price);
-		etStock2Price = (EditText) findViewById(R.id.etStock2Price);
-		etStrikePrice= (EditText) findViewById(R.id.etStrikePrice);
-		etTimetoMaturity= (EditText) findViewById(R.id.etTimetoMaturity); 
-		etSigma1= (EditText) findViewById(R.id.etSigma1);
-		etSigma2= (EditText) findViewById(R.id.etSigma2);
-		etInterestRate= (EditText) findViewById(R.id.etInterestRate);
-		tvResult = (TextView) findViewById(R.id.tvResult);
-		rgOptionType = (RadioGroup) findViewById(R.id.rgOption);
-		rgOptionType.setOnCheckedChangeListener(this);
-		etCovariance = (EditText) findViewById(R.id.etCovariance);
-		etPath = (EditText) findViewById(R.id.etPath);
-		rgMCOption = (RadioGroup) findViewById(R.id.rgMCOption);
-		rgMCOption.setOnCheckedChangeListener(this);
-		
+        spinner = (Spinner) findViewById(R.id.sp_number_of_stock);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.number_of_stocks,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+        int[] spotIds   = {R.id.etStock1Price, R.id.etStock2Price, R.id.etStock3Price, R.id.etStock4Price, R.id.etStock5Price};
+        int[] sigmaIds  = {R.id.etSigma1, R.id.etSigma2, R.id.etSigma3, R.id.etSigma4, R.id.etSigma5};
+
+        etStockPrices = new EditText[spotIds.length];
+        etSigmas      = new EditText[spotIds.length];
+
+        int[] rhoIds    = {
+                R.id.rho_1_1,
+                R.id.rho_2_1,R.id.rho_2_2,
+                R.id.rho_3_1,R.id.rho_3_2,R.id.rho_3_3,
+                R.id.rho_4_1,R.id.rho_4_2,R.id.rho_4_3,R.id.rho_4_4,
+                R.id.rho_5_1,R.id.rho_5_2,R.id.rho_5_3,R.id.rho_5_4,R.id.rho_5_5};
+        etRhos        = new EditText[rhoIds.length];
+        for (int i=0; i<rhoIds.length; i++) {
+            etRhos[i] = (EditText) findViewById(rhoIds[i]);
+        }
+
+        for (int i=0; i<spotIds.length; i++) {
+            etStockPrices[i] = (EditText) findViewById(spotIds[i]);
+            etSigmas[i] = (EditText) findViewById(sigmaIds[i]);
+        }
+
+        bCalculate = (Button) findViewById(R.id.bCalculate);
+        bCalculate.setOnClickListener(this);
+        etStrikePrice= (EditText) findViewById(R.id.etStrikePrice);
+        etTimetoMaturity= (EditText) findViewById(R.id.etTimetoMaturity);
+        etInterestRate= (EditText) findViewById(R.id.etInterestRate);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        rgOptionType = (RadioGroup) findViewById(R.id.rgOption);
+        rgOptionType.setOnCheckedChangeListener(this);
+        etPath = (EditText) findViewById(R.id.etPath);
+        rgMCOption = (RadioGroup) findViewById(R.id.rgMCOption);
+        rgMCOption.setOnCheckedChangeListener(this);
+
+
+
 	    onCheckedChanged(rgMCOption, rgMCOption.getCheckedRadioButtonId());
         onCheckedChanged(rgOptionType, rgOptionType.getCheckedRadioButtonId());
 	}
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+        n = Integer.parseInt(adapterView.getItemAtPosition(pos).toString());
+        int[] linearLayoutIds = {R.id.ll_stock_price1, R.id.ll_stock_price2, R.id.ll_stock_price3, R.id.ll_stock_price4, R.id.ll_stock_price5};
+        int[] tableRowIds = {R.id.table_row1, R.id.table_row2, R.id.table_row3, R.id.table_row4, R.id.table_row5};
+        int[] tableLabelIds = {R.id.table_label_1,R.id.table_label_2,R.id.table_label_3,R.id.table_label_4,R.id.table_label_5};
+
+        for (int i=0; i< linearLayoutIds.length; i++) {
+            LinearLayout ll = (LinearLayout) findViewById(linearLayoutIds[i]);
+            TableRow tr = (TableRow) findViewById(tableRowIds[i]);
+            TextView tv = (TextView) findViewById(tableLabelIds[i]);
+            if (i < n) {
+                if (ll.getVisibility()!=View.VISIBLE) {
+                    ll.setVisibility(View.VISIBLE);
+                }
+
+                if (tr.getVisibility()!=View.VISIBLE) {
+                    tr.setVisibility(View.VISIBLE);
+                }
+
+                if (tv.getVisibility()!=View.VISIBLE) {
+                    tv.setVisibility(View.VISIBLE);
+                }
+
+            } else {
+                if (ll.getVisibility()==View.VISIBLE) {
+                    ll.setVisibility(View.GONE);
+                }
+                if (tr.getVisibility()==View.VISIBLE) {
+                    tr.setVisibility(View.GONE);
+                }
+                if (tv.getVisibility()==View.VISIBLE) {
+                    tv.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
